1046baf26829c0834ec9c583bc11d8ef3f883049
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2016-05-23 15:35:43 -0700

    Environment no longer in gateway
    Stripped in solidus

--- lib/spree_affirm/factories/affirm_payment_method_factory.rb
+++ lib/spree_affirm/factories/affirm_payment_method_factory.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :affirm_payment_method, class: Spree::Gateway::Affirm do
     name "Staging Affirm Split Pay"
     active true
-    environment "test"
     auto_capture false
   end
 end
